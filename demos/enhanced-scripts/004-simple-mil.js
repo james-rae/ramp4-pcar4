af6b84702f3b4f053f3b76542ab4b8080aa9cbbb
James Rae <James.Rae@ec.gc.ca>
2025-01-03 08:16:10 -0500

    fancy demo

--- demos/enhanced-scripts/004-simple-mil.js
+++ demos/enhanced-scripts/004-simple-mil.js
@@ -23,6 +23,7 @@ const runPreTest = (config, options, utils) => {
         sublayers: [
             {
                 index: 0,
+                name: '2022 GRD Source',
                 fixtures: {
                     details: {
                         template: 'RasterGRD'
@@ -65,9 +66,22 @@ const runPostTest = (instance, utils) => {
         },
         template: `
             <div>
-                <span>This is the data.</span>
+                <div v-if="this.loaded" >
                  
-                <p v-if="this.loaded" >{{this.result.value}}</p>
+                
+                    <h3>Cell Value: {{this.result.value}}</h3>
+                    <hr />
+                    <span>Trend Value: 1.2</span>
+                    
+                    <hr />
+                    <h4>Historic Values</h4>
+                    <ul>
+                        <li>2022 --&gt; 1.4</li>
+                        <li>2021 --&gt; 1.2</li>
+                        <li>2020 --&gt; 1.2</li>
+                        <li>2019 --&gt; 1.0</li>
+                    </ul>
+                </div>
                 <p v-else>Loading...</p>
             </div>
         `,
@@ -88,7 +102,7 @@ const runPostTest = (instance, utils) => {
                 this.loaded = false;
                 await this.identifyData.loading;
                 this.result = {
-                    value: JSON.stringify(this.identifyData.data)
+                    value: this.identifyData.data.pixel
                 };
 
                 this.loaded = true;
