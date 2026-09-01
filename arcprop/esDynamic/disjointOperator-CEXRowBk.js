1788abef38f4935fb112faca0aee13456d0f0718
GitHub Action <action@github.com>
2026-09-01 19:19:39 +0000

    Applied changes from demo-folder to demo-page

---
+++ arcprop/esDynamic/disjointOperator-CEXRowBk.js
@@ -0,0 +1,1 @@
+import{v as r}from"./ProjectionTransformation-IkNRcr0S.js";import{fromGeometry as m,fromGeometryToGXGeometry as n}from"./jsonConverter-BYgg06MF.js";const f=new r;function s(t,o){const e=m(t);return f.execute(e.getGeometry(),n(o),e.getSpatialReference())}export{s as execute};
